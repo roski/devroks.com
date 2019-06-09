0311a62a05678d11d3e0f791c3db3f6b45d117df
Roman Skydan <roma.skidan@gmail.com>
2019-06-09 15:53:14 +0300

    Change sass to scss

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'dr-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.sass']
+  styleUrls: ['./admin-panel.component.scss']
 })
 export class AdminPanelComponent implements OnInit {
 
